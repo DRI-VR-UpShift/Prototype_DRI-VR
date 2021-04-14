40b433f3dc771c34b50795aaa7e32c68e57647fe
Tommaso Giulietti <45176602+Tomm4so@users.noreply.github.com>
2021-04-14 14:31:40 +0200

    test
    test

--- Prototype/Assets/Scriots/CameraScript.cs
+++ Prototype/Assets/Scriots/CameraScript.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    float speed = 50.0f;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
 
     // Use this for initialization
     void Start()
@@ -12,19 +19,14 @@ public class CameraScript : MonoBehaviour
 
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Mouse X") > 0)
-        {
-            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
-                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
-        }
 
-        else if (Input.GetAxis("Mouse X") < 0)
-        {
-            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
-                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
-        }
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
     }
-
 }
