2114fc0f3cad069903d01fa5eacb2f86c513cf2c
Tommaso Giulietti <45176602+Tomm4so@users.noreply.github.com>
2021-04-21 12:31:18 +0200

    Fix Camera & Video Flip? Maybe

--- Prototype/Assets/Scriots/CameraScript.cs
+++ Prototype/Assets/Scriots/CameraScript.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    float speed = 50.0f;
+  
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
 
     // Use this for initialization
     void Start()
@@ -14,17 +19,11 @@ public class CameraScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetAxis("Mouse X") > 0)
-        {
-            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
-                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
-        }
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
 
-        else if (Input.GetAxis("Mouse X") < 0)
-        {
-            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
-                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
-        }
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
+
 
 }
